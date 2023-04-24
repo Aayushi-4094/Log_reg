a422a8138701ceeff39ab95f800e2d6bad6d2d33
Aayushi <aayushiagarwal4094@gmail.com>
2023-04-24 20:13:21 +0530

    change in homepage

--- static/script.js
+++ static/script.js
@@ -4,12 +4,12 @@
 const cards = document.querySelectorAll('.dashboard-card');
 
 // Add a click event listener to each card
-cards.forEach(card => {
-  card.addEventListener('click', () => {
-    // Get the card title
-    const title = card.querySelector('h2').textContent;
+cards.forEach(function (card) {
+    card.addEventListener('click', () => {
+      // Get the card title
+      const title = card.querySelector('h2').textContent;
 
-    // Show an alert with the card title
-    alert(`You clicked on the "${title}" card!`);
+      // Show an alert with the card title
+      alert(`You clicked on the "${title}" card!`);
+    });
   });
-});
